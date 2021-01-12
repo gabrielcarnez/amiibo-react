8381c3032b4d853b7005fc1711a8f097a69c3695
Gabriel David Carnez <gabriel.carnez@gmail.com>
2021-01-11 23:29:09 -0300

    Delete amiibo-react-main directory

--- amiibo-react-main/src/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import fetchAmiibos from "./reducers/fetchAmiibos";
-
-const rootReducer = combineReducers({
-	fetchAmiibos,
-});
-
-export default rootReducer;
